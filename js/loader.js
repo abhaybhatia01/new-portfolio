ea78391ea39c6ee51136e0c7f993eda86a7a217b
Abhay Bhatia <abhay1732002@gmail.com>
2023-07-30 22:53:02 +0530

    theme module complete

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,8 @@
 window.addEventListener("load", function finishPreload() {
+    // setting the theme
+    setTheme();
+
+
     //hiding the loader
     const loader = document.querySelector(".loader");
     loader.classList.add("loader-hidden");
@@ -8,6 +12,36 @@ window.addEventListener("load", function finishPreload() {
         bubble.classList.remove("loading-active");
     });
 });
+
+
+
+
+
+function setTheme(changeTo) {
+    const theme = localStorage.getItem('color-scheme')
+    const root = document.querySelector('.root')
+    if(theme === "dark"){
+        console.log("dark found")
+        root.classList.add("dark-theme")
+        root.classList.remove('light-theme')
+    }
+    else if(theme === "light"){
+        console.log("light found")
+        root.classList.add("light-theme")
+        root.classList.remove('dark-theme')
+    }
+    else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // Dark mode is preferred
+        console.log("dark prefers")
+        root.classList.add('dark-theme')
+        root.classList.remove('light-theme')
+    } else {
+    // Light mode is preferred
+        console.log("light prefers")
+        root.classList.add('light-theme')
+        root.classList.remove('dark-theme')
+    }
+}
 // document
 //     .querySelector(".img")
 //     .addEventListener("click", function preload() {
